87d8292170b3c753c4a44061c4657c03f0ab6d60
6uzmen <6uzmen@users.noreply.github.com>
2020-06-16 12:38:26 -0300

    Update

--- controllers/tweetsController.js
+++ controllers/tweetsController.js
@@ -1,9 +1,8 @@
-const { Tweet } = require("../data/models");
+const { Tweet, User } = require("../data/models");
 
 module.exports = {
   // GET: /api/tweets?skip={Number}&limit{Number}
   tweets(req, res, next) {
-      
     let { skip = 0, limit = 20 } = req.query;
 
     skip = Number(skip);
@@ -20,6 +19,33 @@ module.exports = {
     ])
       .then(([tweets, total]) => {
         res.json({ tweets, hasMore: skip + limit < total });
+      })
+      .catch(next);
+  },
+  myTweets(req, res, next) {
+
+    let username = req.query.username;
+    let userId;
+    let { skip = 0, limit = 20 } = req.query;
+
+    skip = Number(skip);
+    limit = Number(limit);
+
+    Promise.all([User.findOne({ username })]).then(([userData]) => userId = userData._id);
+
+    Promise.all([
+      Tweet.find({author:userId})
+      //   .sort({ createdAt: "desc" })
+      //   .skip(skip)
+      //   .limit(limit)
+      //   .populate("author")
+      //   .exec(),
+      // Tweet.countDocuments(),
+    ])
+      .then(([tweets, total]) => {
+        console.log(tweets);
+        res.json({ tweets, hasMore: skip + limit < total });
+        console.log(total + " a")
       })
       .catch(next);
   },
--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,6 @@ const tweetsController = require("../controllers/tweetsController");
 const router = express.Router();
 
 router.get("/tweets", tweetsController.tweets);
-
-
+router.get("/myTweets", tweetsController.myTweets);
 
 module.exports = router;
