78c31b0b3bfbcf986f7e2cd7728ea0822389e2fd
Camilo Sasía <18406325+camilosasia@users.noreply.github.com>
2020-06-19 11:33:05 -0300

    Agrega controlador postTweet

--- controllers/tweetsController.js
+++ controllers/tweetsController.js
@@ -2,7 +2,7 @@ const { Tweet, User } = require("../data/models");
 
 module.exports = {
   // GET: /api/tweets?skip={Number}&limit{Number}
-  
+
   tweets(req, res, next) {
     let { skip = 0, limit = 100 } = req.query;
 
@@ -10,40 +10,41 @@ module.exports = {
     limit = Number(limit);
 
     Promise.all([
-      Tweet.find()
-        .sort({ createdAt: "desc" })
-        .populate("author")
-        .exec(),
+      Tweet.find().sort({ createdAt: "desc" }).populate("author").exec(),
       Tweet.countDocuments(),
     ])
       .then(([tweets, total]) => {
-        res.set('Access-Control-Allow-Origin', '*');
+        res.set("Access-Control-Allow-Origin", "*");
         res.json({ tweets, hasMore: skip + limit < total });
       })
       .catch(next);
   },
-  createTweet(req,res,){
 
-    let username = req.query.username;
-    let content = req.query.content;
+  postTweet(req, res) {
+    const { author, content } = req.body;
 
-    isUndefined(username,"Username",res);
-    isUndefined(content,"Content",res);
+    if (!author) {
+      return res.status(400).json({ error: "author missing" });
+    }
 
-    User.findOne({username: username})
-    .then()
+    if (!content) {
+      return res.status(400).json({ error: "content missing" });
+    }
 
+    Tweet.create({ author, content }).then((savedTweet) => {
+      response.json(savedTweet);
+    });
   },
-  userTweets(req, res, next) {
 
+  userTweets(req, res, next) {
     let username = req.query.username;
-    let page = !req.query._page ? 0 : req.query._page 
-    let limit = !req.query._limit ? 10 : req.query._limit 
+    let page = !req.query._page ? 0 : req.query._page;
+    let limit = !req.query._limit ? 10 : req.query._limit;
 
     page = Number(page);
     limit = Number(limit);
 
-    isUndefined(username,"Username",res);
+    isUndefined(username, "Username", res);
 
     Promise.all([User.findOne({ username })]).then(([userData]) => {
       Promise.all([
@@ -60,54 +61,46 @@ module.exports = {
     });
   },
   followingTweets(req, res) {
-
     let username = req.query.username;
-    let page = !req.query._page ? 0 : req.query._page 
-    let limit = !req.query._limit ? 10 : req.query._limit 
+    let page = !req.query._page ? 0 : req.query._page;
+    let limit = !req.query._limit ? 10 : req.query._limit;
     let followersTweets = [];
 
     page = Number(page);
     limit = Number(limit);
 
-    isUndefined(username,"Username",res);
+    isUndefined(username, "Username", res);
 
     User.findOne({ username }).then((userData) => {
-
-      isUndefined(userData,"La informacion del usuario",res);
+      isUndefined(userData, "La informacion del usuario", res);
 
       const getFollowersTweets = async () => {
         for (let i = 0; i < userData.following.length; i++) {
           await Tweet.find({ author: userData.following[i] })
-          .sort({ createdAt: "desc" })
-          .skip(page * limit)
-          .limit(limit)
-          .populate("author")
-          .then(
-            (followerData) => {
+            .sort({ createdAt: "desc" })
+            .skip(page * limit)
+            .limit(limit)
+            .populate("author")
+            .then((followerData) => {
               followersTweets = [...followersTweets, ...followerData];
-            }
-          );
+            });
         }
       };
 
       getFollowersTweets().then(() => {
-
         //Sort tweets by Date
-        followersTweets.sort(
-          function(a, b){
-            return Date.parse(b.createdAt) - Date.parse(a.createdAt)
-          });
-         res.set('Access-Control-Allow-Origin', '*');
-         res.send(followersTweets.slice(0,limit));
-        
+        followersTweets.sort(function (a, b) {
+          return Date.parse(b.createdAt) - Date.parse(a.createdAt);
+        });
+        res.set("Access-Control-Allow-Origin", "*");
+        res.send(followersTweets.slice(0, limit));
       });
-      
     });
   },
 };
 
-  const isUndefined = (element,elementName,res) => {
-    if(!element){
-      return res.json({error:`${elementName} es invalide. ${element}`})
-    }
+const isUndefined = (element, elementName, res) => {
+  if (!element) {
+    return res.json({ error: `${elementName} es invalide. ${element}` });
   }
+};
