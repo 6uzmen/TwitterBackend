d20f03054ea49230e6cbed60991d48dea01a765c
Camilo Sasía <18406325+camilosasia@users.noreply.github.com>
2020-06-19 11:30:28 -0300

    Muestra el número de puerto por consola

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 require("dotenv").config();
 
 const express = require("express");
-var cors = require('cors')
+const cors = require("cors");
+
 const app = express();
 
 const apiRouter = require("./routes/api");
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cors())
-app.use("/api",apiRouter);
+app.use("/api", apiRouter);
 
-
-app.listen(3002)
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
